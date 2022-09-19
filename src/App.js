8b1af80452319a1cc914f0db12413d824df8116a
kyj1212 <yongjun3002@naver.com>
2022-09-20 01:54:23 +0900

    personal peoject modify(2)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function Movie(){
   const [command,setComand] = useState("")
   const [attend,setAttend] = useState([])
   
+  
   function introduce(event){
     event.preventDefault()
     setSubject("이 영화는 상당히 재밌습니다")
@@ -25,30 +26,37 @@ function Movie(){
   }
   function onChange(event){
     setComand(event.target.value)
-    console.log(command)
   }
-  function sum(event){
-    setAttend()
+  function onSubmit(event){
+    event.preventDefault(attend,command)
+    setAttend(function(){
+      return [command, ...attend] // comand가 인자로 안들어갔는데 comand가 나오는게 궁금
+    })
   }
- 
-
+  
   return(
     <div>
-      <img id='movie_moun' src='mountain.jpg'></img>
-      <ul>
-        <li><h2><a href='/home'>홈으로</a></h2></li>
-        <li><a onClick={introduce} href=''>영화소개</a></li>
-        <li><a onClick={person} href=''>등장인물</a></li>
-        <li><a onClick={story} href=''>줄거리</a></li>
-      
-      </ul>
-      <h1>{subject}</h1>
-      <h2>{subtitle}</h2>
+      <div id='asd'>
+        <img id='movie_moun' src='mountain.jpg'></img>
+      </div>
+        <div id='home'><h2><a href='/home' id='home_id'>홈으로</a></h2></div>
+        <div id='introduce'>
+          <a id='intro_intro' onClick={introduce} href=''>영화소개</a>
+          <a id='intro_person' onClick={person} href=''>등장인물</a>
+          <a id='intro_story' onClick={story} href=''>줄거리</a>
+        </div>
+      <div id='intro_output'>
+        <h1>{subject}</h1>
+        <h2>{subtitle}</h2>
+      </div>
       
       <h3>리뷰달기</h3>
-      <input value={command} type='text' placeholder='clean봇이 감지중입니다' onChange={onChange}>
-      </input>
-      <button onClick={sum}>댓글추가</button>
+      <form onSubmit={onSubmit}>
+        <input value={command} type='text' placeholder='clean봇이 감지중입니다' onChange={onChange}>
+        </input>
+        <button>댓글추가</button>
+      </form>
+      <h3>{attend}</h3>
     </div>
   )
 }
