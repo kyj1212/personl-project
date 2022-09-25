78bd556f31ea1be1f115fa43745ad3fc1c34a9db
kyj1212 <yongjun3002@naver.com>
2022-09-25 22:34:25 +0900

    9/25 pro

--- src/axios.js
+++ src/axios.js
@@ -1,36 +1,28 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
 import axios from 'axios'
 
-const a = axios.post("http://58.229.6.137:3200/auth/signin",
-{
+const a = axios.post("http://58.229.6.137:3200/auth/signup",
+    {
   "email": "string",
-  "password": "string"
-}).then((res)=>{
-    console.log(res)
-}).catch((error) => {
-    console.log(error.response);
-  });
-
-const b = axios.get("http://58.229.6.137:3200/user",
-{
-  "id": 0,
+  "password": "string",
   "name": "string",
-  "email": "string",
-  "weeklyDailyCount": 0,
-  "recentPositiveAvgRating": 0
-}).then((res)=>{
-    console.log(res)
-}).catch((error) => {
-    console.log(error.response);
-  })
+  "age": 0,
+  "phone": "string",
+  "isAgreedCounseling": false,
+  "isAgreedPrivacy": false
+    }
+).then(function a(response) { 
+	console.log(response) 
+})
+.catch(function (error) {
+	console.log(error);
+});
 
 function Axios(){
     return(
         <div>
             {a()}
-            {b()}
-            
-        </div>
+        </div>       
     )
 }
 export default Axios
