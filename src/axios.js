1ce315f10289b59ab433b98f11fa22d919618cb9
kyj1212 <yongjun3002@naver.com>
2022-09-26 23:07:35 +0900

    926 mod

--- src/axios.js
+++ src/axios.js
@@ -2,62 +2,41 @@ import React, {useEffect,useState} from "react";
 import axios from 'axios'
 
 
-function Enroll(){
 
-    const [email,setEmail] = useState("")
-    function onChange(event){
-        setEmail(event.target.value)
-    }
-    const a = axios.post("http://58.229.6.137:3200/auth/signup",
-    {
-    "email": email ,
-    "password": "string",
-    "name": "string",
-    "age": 0,
-    "phone": "string",
-    "isAgreedCounseling": false,
-    "isAgreedPrivacy": false
-    }
-    ).then(function (response) { 
-        console.log(response) 
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
 
-    return(
-        <div>
-            {a}
-            <input onChange={onChange} value={email}></input>
-        </div>
-    )
+    
+
+
+
+
+
+
+
+const a = axios.post("http://58.229.6.137:3200/auth/signup",
+{
+"email": "string" ,
+"password": "string",
+"name": "string",
+"age": 0,
+"phone": "string",
+"isAgreedCounseling": false,
+"isAgreedPrivacy": false
 }
-function Login(){
-  const b = axios.post("http://58.229.6.137:3200/auth/signin",{
-    "email": "string",
-  "password": "string"
-  }).then(function (response) { 
+).then(function (response) { 
     console.log(response) 
 })
 .catch(function (error) {
     console.log(error);
-})
-
-return(
-    {b}
-)
-
-}
+});
 
 
-function Axios(){
+function A(){
 
    
     return(
         <div>
-            <Enroll></Enroll>
-            <Login></Login>
+            {a}
         </div>       
     )
 }
-export default Axios;
+export default A;
