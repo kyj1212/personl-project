0678a4899ea418386f1647277081dad3671e05d5
kyj1212 <yongjun3002@naver.com>
2022-10-01 20:29:22 +0900

    10/1 pro

---
+++ src/axi component/login.js
@@ -0,0 +1,44 @@
+import React, {useEffect,useState} from "react";
+import axios from 'axios'
+
+
+    function Login(){
+    
+
+        const [email_login,setEmail_login] = useState("")
+        const [password_login,setPassword_login] = useState("")
+    
+        function loginsend(event){
+           const a = axios.post("http://58.229.6.137:3200/auth/signin",
+            {
+                "email": email_login,
+                "password": password_login 
+            }).then(function (res) { 
+                return res.data.token
+            }).catch(function (error) {
+                console.log(error);
+            });
+            console.log(a)
+        }
+
+
+        
+        return(
+            <div>
+                <p>
+                    <h3>로그인</h3>
+                </p>
+                <p>
+                    <input onChange={(event)=>{setEmail_login(event.target.value)}} value={email_login} placeholder='email'></input>
+                </p>
+                <p>
+                    <input onChange={(event)=>{setPassword_login(event.target.value)}} value={password_login} placeholder='password'></input>
+                </p>
+                <button onClick={loginsend}>보내기</button>
+            </div>
+    
+        )
+    }
+
+ export default Login;
+       
